47a69aa18a7ff1f59ce4cdb06cef8d428143f78d
Nick F <nick.ferrazza92@gmail.com>
2018-10-27 09:25:38 -0400

    final

---
+++ client/app/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer>
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    
+    
+    <p>Nick Ferrazza 2018</p>
+  </footer>
+);
+
+export default Footer;
